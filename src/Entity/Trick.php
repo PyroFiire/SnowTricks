20283e4fc2463be3766f848dee059361433cc089
Christophe Guinot <christophe.guinot@hotmail.fr>
2019-09-05 15:12:29 +0200

    the title Trick is Unique and the error is display

--- src/Entity/Trick.php
+++ src/Entity/Trick.php
@@ -4,10 +4,13 @@ namespace App\Entity;
 
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\TrickRepository")
+ * @UniqueEntity("title")
  */
 class Trick
 {
