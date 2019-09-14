1ed309f28f110ea04bb35226ee759a46ad947b85
Christophe Guinot <christophe.guinot@hotmail.fr>
2019-09-14 20:53:53 +0200

    send mail with smtp gmail if user sign in

--- src/Controller/Users/RegistrationController.php
+++ src/Controller/Users/RegistrationController.php
@@ -3,21 +3,25 @@
 namespace App\Controller\Users;
 
 use App\Entity\User;
-use App\Form\RegistrationType;
-
 use Twig\Environment;
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\RedirectResponse;
-
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
-
-use Symfony\Component\Form\FormFactoryInterface;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use App\Form\RegistrationType;
 
 use Doctrine\ORM\EntityManagerInterface;
+
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
+
+use Symfony\Component\HttpFoundation\Response;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
 
 class RegistrationController
 {
@@ -64,7 +68,7 @@ class RegistrationController
     /**
      * @Route("/registration", name="user_registration")
      */
-    public function registration(Request $request)
+    public function registration(Request $request, MailerInterface $mailer)
     {
         $formRegistration = $this->form->create(RegistrationType::class, $user = new User());
 
@@ -76,6 +80,22 @@ class RegistrationController
             //$user->setActiveToken
             $user->setActive(false);
             $user->setPicturePath('default_avatar.jpg');
+
+            $email = (new TemplatedEmail())
+                ->from('christophe.guinot@hotmail.fr')
+                ->to('christophe.guinot@hotmail.fr')
+                ->subject('Active your account !')
+
+                // path of the Twig template to render
+                ->htmlTemplate('emails/registration.html.twig')
+
+                // pass variables (name => value) to the template
+                ->context([
+                    'username' => $user->getUsername(),
+                ])
+            ;
+            
+            $mailer->send($email);
             $this->manager->persist($user);
             $this->manager->flush();
 
