a6800a92646ac4c018a76ddb587f0ee5b8f2aeca
Christophe Guinot <christophe.guinot@hotmail.fr>
2019-10-08 12:52:51 +0200

    factoring js code for the input change event

--- public/js/spotlightFileUpload.js
+++ public/js/spotlightFileUpload.js
@@ -1,20 +1,29 @@
 //Display:none the basique input
 $("#trick_fileSpotlightPicturePath").parent().addClass('d-none');
 
-// For upload file
+// clic button for upload file
 $("#spotlightFileUpload").on('click', function(e){
-$('#trick_fileSpotlightPicturePath').click();
-return false;
+    $('#trick_fileSpotlightPicturePath').click();
+    return false;
 });
 
 //for delete current file if he was uploaded
 var defaultSpotlight = '/picture/snowboarding-3554x1999.jpg';
 //var defaultSpotlight = $('#spotlight_picture').attr('src');
 
+// clic button for reset the file input
 $("#spotlightFileDelete").on('click', function(e){
-$("#trick_fileSpotlightPicturePath").attr('type', 'text').attr('type', 'file');
-$('#spotlight_picture').attr('src', defaultSpotlight);
-return false;
+    $("#trick_fileSpotlightPicturePath")
+        .attr('type', 'text')
+        .attr('type', 'file');
+    $('#spotlight_picture')
+        .attr('src', defaultSpotlight);
+    return false;
+});
+
+// preview image if is changed input
+$("#trick_fileSpotlightPicturePath").on('change', function(){
+    readURL(this);
 });
 
 //preview the upload file
