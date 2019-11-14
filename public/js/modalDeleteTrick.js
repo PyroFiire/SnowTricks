181d0fa3dd57edde55059351e9aa517beba0e65a
Christophe Guinot <christophe.guinot@hotmail.fr>
2019-11-14 13:31:18 +0100

    add semicolon

--- public/js/modalDeleteTrick.js
+++ public/js/modalDeleteTrick.js
@@ -5,7 +5,7 @@ $("#modalDeleteTrick").on("show.bs.modal", function (event) {
     // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
     // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
     var modal = $(this);
-    modal.find(".modal-body").text("Are you sure to remove \"" + title + "\"")
+    modal.find(".modal-body").text("Are you sure to remove \"" + title + "\"");
     var modal2 = $(this);
-    modal2.find(".modal-footer a").attr("href", "/tricks/delete/" + slug)
-})
+    modal2.find(".modal-footer a").attr("href", "/tricks/delete/" + slug);
+});
